72ac56ee5fe019b4a6332fd08d3916b5d052b3f4
sunil <sunilkumardeeple2022@gmail.com>
2024-01-25 14:55:22 +0530

    Initial Commit

---
+++ BridgeLab_Bootcamp/src/Addition.java
@@ -0,0 +1,14 @@
+
+public class Addition {
+   static int a=20;
+   static int b=30;
+   static int result =a+b;
+   
+   
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("The result will be"+ "" + result);
+	}
+
+}
+                   
---
+++ BridgeLab_Bootcamp/src/Multiplication.java
@@ -0,0 +1,12 @@
+
+public class Multiplication {
+      static int a= 20;
+      static int b =3;
+      static int result = (a*b);
+      
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println( "The answer is " + result);
+	}
+
+}
